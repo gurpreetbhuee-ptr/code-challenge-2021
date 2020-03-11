372a3460420b899b7a3c8bd8a27aa75cf7cfa15c
Gurpreet Singh <g.bhuee@reply.com>
2020-03-11 17:07:21 +0000

    made more generic

--- src/reply/codechalange/main/InputReader.java
+++ src/reply/codechalange/main/InputReader.java
@@ -28,6 +28,7 @@ public class InputReader
 	public static void main(final String[] args)
 	{
 		terrainValueMap = getTerrainMap();
+		//todo change the file with the input file
 		final File f = new File("resources/1_victoria_lake.txt");
 		try
 		{
@@ -35,7 +36,7 @@ public class InputReader
 			final Scanner sc = new Scanner(inputStream, "UTF-8");
 
 			System.out.println("Start reading file......");
-
+			//todo add the traversing the file logic understanding the input file
 
 			final String firstLine = sc.nextLine();
 			final String[] firstArr = firstLine.split("\\s");
@@ -52,7 +53,7 @@ public class InputReader
 				final String cutomerOffice = sc.nextLine();
 				final String[] arrCustOffice = cutomerOffice.split("\\s");
 
-				final Point point = new Point(Integer.parseInt(arrCustOffice[0]) -1, Integer.parseInt(arrCustOffice[1]) -1, null);
+				final Point point = new Point(Integer.parseInt(arrCustOffice[0]) - 1, Integer.parseInt(arrCustOffice[1]) - 1, null);
 				final Customer customer = new Customer(customerId, point,
 						Integer.parseInt(arrCustOffice[2]));
 				customers.add(customer);
@@ -69,31 +70,18 @@ public class InputReader
 				final String line = sc.nextLine();
 				for (int i = 0; i < n; i++)
 				{
-					grid[countRow][i]= terrainValueMap.get(line.charAt(i));
+					grid[countRow][i] = terrainValueMap.get(line.charAt(i));
 				}
 				countRow++;
 			}
 
 
 			System.out.println("Finished reading file......");
-//			System.out.println(intGrid);
-
-			final GraphTraversal graphTraversal = new GraphTraversal();
-
-			System.out.println("Start traversing graph ....");
-			final List<RouteIterator> routeIterators = graphTraversal.traverseGraph(grid, customers);
-
-			//create Route iterator for each coordinate in the grid
-
-			//Iterate the Route iterator to find the apt offices
-
-			//populate the Output object
-
-			//convert the output object to a file
 
 
-			final Output student = new Output(1, 2, "uuuddddrrrlll");
-			//			WriteObjectToFile(student);
+			System.out.println("Start converting response ....");
+
+			WriteObjectToFile(new ArrayList<>());
 		}
 		catch (final FileNotFoundException e)
 		{
@@ -117,21 +105,19 @@ public class InputReader
 	}
 
 
-	public void WriteObjectToFile(final Output result)
+	public static void WriteObjectToFile(final List<Object> objects)
 	{
 		try
 		{
-			// approach 1
-			final FileOutputStream fileOut = new FileOutputStream("resources/result.txt");
-			final ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
-			objectOut.writeObject(result);
-			objectOut.close();
-			fileOut.close();
-			// approach 2
-			final PrintWriter out = new PrintWriter(new FileWriter("resources/output.txt", true), true);
-			out.write(result.getX() + " " + result.getY() + " " + result.getPath());
-			out.close();
+			final PrintWriter out = new PrintWriter(new FileWriter("resources/output.txt", false), true);
+			out.write(" Test" + " Value");
+			out.write(objects.size() + "\n");
+			objects.forEach(result ->
+			{
+				out.write(" " + "  ");
 
+			});
+			out.close();
 			System.out.println("The Object  was succesfully written to a file");
 
 		}
